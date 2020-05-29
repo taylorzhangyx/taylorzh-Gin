ba740c321dc94f11cb114cac29d8cfd9860fc25a
YuxinZhang <taylorzhangyx@gmail.com>
2020-05-28 18:32:01 -0700

    create / endpoint
    Signed-off-by: YuxinZhang <taylorzhangyx@gmail.com>

--- main.go
+++ main.go
@@ -4,10 +4,16 @@ import "github.com/gin-gonic/gin"
 
 func main() {
 	r := gin.Default()
+	r.GET("/", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "hello world",
+		})
+	})
+
 	r.GET("/ping", func(c *gin.Context) {
 		c.JSON(200, gin.H{
 			"message": "pong",
 		})
 	})
-	r.Run(":2020") // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
+	r.Run(":2020")
 }
